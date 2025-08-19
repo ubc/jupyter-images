4e1f21c8269f88cdf70c6ea4289033c047ee7ea4
Ryoko Norden <ryoko.norden@gmail.com>
2025-08-19 14:17:16 -0700

    Change python libray installation to install-common.sh

---
+++ install-common.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+
+echo "Upgrading setuptools and pip..."
+pip install --upgrade pip setuptools
+
+echo "Installing common Python packages..."
+pip install nbgitpuller \
+    jupyterlab-code-formatter \
+    jupyterlab-spreadsheet-editor \
+    jupyterlab_templates \
+    otter-grader \
+    vl-convert-python \
+    "vegafusion[embed]" \
+    "vegafusion-jupyter[embed]"
+
+echo "Installation complete!"
