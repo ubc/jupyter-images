2c4c262d101f31d83a11e54b7c6b49dd50594f36
Ryoko Norden <ryoko.norden@gmail.com>
2026-02-03 18:58:13 +0000

    Fix .ipynb_checkpoint not found error on assignment collection with ngsshare exchange'

--- tensorflow-notebook/nbgrader_config.py
+++ tensorflow-notebook/nbgrader_config.py
@@ -8,3 +8,11 @@ configureExchange(
 # Add the following to let students access courses without configuration
 # For more information, read Notes for Instructors in the documentation
 c.CourseDirectory.course_id = '*'
+
+# Bypass checkpoint files
+c.Exchange.exclude = [
+    ".ipynb_checkpoints",
+    ".ipynb_checkpoints/**",
+    "__pycache__",
+    "*.pyc",
+]
