4371c408380b8de33946ccee94c48f33da19122f
Ryoko Norden <ryoko.norden@ubc.ca>
2026-04-20 14:59:11 -0700

    Revert back the global nbgrader_config.py contents due to the error

--- tensorflow-notebook/nbgrader_config_global.py
+++ tensorflow-notebook/nbgrader_config_global.py
@@ -4,3 +4,15 @@ c = get_config()
 configureExchange(
     c, 'http://ngshare.default.svc.cluster.local:8080/jupyter/services/ngshare'
 )
+
+# Add the following to let students access courses without configuration
+# For more information, read Notes for Instructors in the documentation
+c.CourseDirectory.course_id = '*'
+
+# Bypass checkpoint files
+c.Exchange.exclude = [
+    ".ipynb_checkpoints",
+    ".ipynb_checkpoints/**",
+    "__pycache__",
+    "*.pyc",
+]
