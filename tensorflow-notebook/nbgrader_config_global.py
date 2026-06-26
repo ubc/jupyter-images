3bf51762788381d9952253191d544db2701aff89
Ryoko Norden <ryoko.norden@ubc.ca>
2026-06-26 11:47:07 -0700

    Remove /jupyter baseURL from ngshare service

--- tensorflow-notebook/nbgrader_config_global.py
+++ tensorflow-notebook/nbgrader_config_global.py
@@ -2,5 +2,5 @@ from ngshare_exchange import configureExchange
 
 c = get_config()
 configureExchange(
-    c, 'http://ngshare.default.svc.cluster.local:8080/jupyter/services/ngshare'
+    c, 'http://ngshare.default.svc.cluster.local:8080/services/ngshare'
 )
